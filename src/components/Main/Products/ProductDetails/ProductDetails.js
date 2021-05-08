de01543c46d423b600a947f2dd3c3c1a7257dff6
Drolpik <mrbarti98@gmail.com>
2021-05-08 19:38:57 +0200

    Added specyfications box to ProductDetails

--- src/components/Main/Products/ProductDetails/ProductDetails.js
+++ src/components/Main/Products/ProductDetails/ProductDetails.js
@@ -25,7 +25,36 @@ const ProductDetails = () => {
       </div>
 
       <div className={classes.Description}>
+        <div>
+          <h3>Description</h3>
+        </div>
         <p>{product.description}</p>
+      </div>
+
+      <div className={classes.Specifications}>
+        <div>
+          <h3>Specifications</h3>
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Memory</td>
+              <td>{product.memory}</td>
+            </tr>
+            <tr className={classes.EvenRow}>
+              <td>Battery</td>
+              <td>{product.battery}</td>
+            </tr>
+            <tr>
+              <td>Connectivity</td>
+              <td>{product.connectivity}</td>
+            </tr>
+            <tr className={classes.EvenRow}>
+              <td>Case size</td>
+              <td>{product.caseSize}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
